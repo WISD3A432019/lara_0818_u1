73795dcddc96320f962525e75d3621022af112e0
jkluio132465 <jkluio132465@gmail.com>
2018-01-02 23:41:07 +0800

    註冊 Service Provider

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Recca0120\LaravelTracy\ServiceProvider::class,
 
         /*
          * Package Service Providers...
