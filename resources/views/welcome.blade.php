d170effed00ab363693254b478c361263acd7141
jkluio132465 <jkluio132465@gmail.com>
2018-01-03 00:28:28 +0800

    加入QRcode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,8 +88,13 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div class="visible-print text-center">
+                     {!! QrCode::size(100)->generate(Request::url()); !!}
+                    <p>Scan me to return to the original page.</p>
+                    </div>
                 </div>
             </div>
         </div>
+        
     </body>
 </html>
